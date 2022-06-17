065a2d8fb9637a76ccdbc98043c6fc3f38ef1276
Monk <anhmonkdv@gmail.com>
2022-06-17 17:06:26 +0700

    attribute fix

--- js/account_validator.js
+++ js/account_validator.js
@@ -1,40 +1,30 @@
 import accs from  "https://combinatronics.com/Henrily041103/OLAR_test/main/test_acc/acc.json";
 
 class account {
-    #username;
-    #password;
-    #type;
+    username;
+    password;
+    type;
 
     constructor(u, p, t) {
-        this.#username = u;
-        this.#password = p;
-        this.#type = t;
-    }
-
-    get #username() {
-        return this.#username;
-    }
-    get #password() {
-        return this.#password;
-    }
-    get #type() {
-        return this.#type;
+        this.username = u;
+        this.password = p;
+        this.type = t;
     }
 }
 
 export class acc_manager {
-    #acc_list = new Array<account>(accs.length);
+    acc_list = new Array<account>(accs.length);
 
     constructor() {
         var account_list = JSON.parse(accs.accounts);
         for (var i = 0; i<accs.length;i++) {
-            this.#acc_list = account(account_list[i].username, account_list[i].password, account_list[i].type);
+            this.acc_list = account(account_list[i].username, account_list[i].password, account_list[i].type);
         }
     }
 
     isValidAccount(u, p) {
-        for (var stored_acc in this.#acc_list) {
-            if ((u == stored_acc.#username) && (p == stored_acc.#password)) {
+        for (var stored_acc in this.acc_list) {
+            if ((u == stored_acc.username) && (p == stored_acc.password)) {
                 return true;
             }
         }
@@ -42,8 +32,8 @@ export class acc_manager {
     }
 
     checkDuplicate(u, p) {
-        for (var stored_acc in this.#acc_list) {
-            if ((u == stored_acc.#username) && (p == stored_acc.#password)) {
+        for (var stored_acc in this.acc_list) {
+            if ((u == stored_acc.username) && (p == stored_acc.password)) {
                 return true;
             }
         }
